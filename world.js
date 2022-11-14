74dc5cf8f945ba6ee11c78de44ffbb9f25444629
Gabriell Gardner <gabriell.github@gmail.com>
2022-11-14 15:55:00 -0500

    Display cities in a country

--- world.js
+++ world.js
@@ -10,7 +10,42 @@ window.onload = function()
 
         var countryQuery = document.getElementById('country').value;
         var url = "world.php?country=" + countryQuery;
-        //var url = "http://localhost/info2180-lab5/world.php?country" + countryQuery;
+
+        httprequest.onreadystatechange = getCountry;
+        httprequest.open('GET', url, true);
+        httprequest.send();    
+
+    });
+
+    function getCountry() {
+
+        if (httprequest.readyState === XMLHttpRequest.DONE) {
+
+            if(httprequest.status === 200) {
+
+                var response = httprequest.responseText;
+                var searchResult = document.querySelector('#result');
+                searchResult.innerHTML = response;
+
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+
+
+
+
+    var lookupCity = document.querySelector('#lookupCities');
+    var httprequest;
+
+    lookupCity.addEventListener('click', function(element){
+
+        element.preventDefault();
+        httprequest = new XMLHttpRequest();
+
+        var countryQuery = document.getElementById('country').value;
+        var url = "world.php?lookUp=" + countryQuery;
 
         httprequest.onreadystatechange = getCountry;
         httprequest.open('GET', url, true);
--- world.php
+++ world.php
@@ -7,13 +7,31 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 //$stmt = $conn->query("SELECT * FROM countries");
 $country = $_GET['country'];
+$lookUp = $_GET['country'];
+
+
+if (isset($lookUp) || !empty($lookUp)) {
+
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON countries.code=cities.country_code");
+    $cityResult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+
+
+
 
 if (isset($country) || !empty($country)) {
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+
 }
+
+
 
 
 ?>
@@ -41,12 +59,21 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-
-
-
-
-
-
-
-
-
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($cityResult as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['district']; ?></td>
+            <td><?= $row['population']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
